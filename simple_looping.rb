18780c04ddbad9dd85c5cf1439eb2aeebf20b9fa
Ryan Hu <ryanhuuu@gmail.com>
2018-03-17 23:13:32 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,9 +11,7 @@ end
 def times_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times do
-    puts phrase
-  end
+
 end
 
 def while_iterator(number_of_times)
