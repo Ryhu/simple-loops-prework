904d9528b036520b58a811260814bf269f4871b5
Ryan Hu <ryanhuuu@gmail.com>
2018-03-17 23:11:17 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,30 +1,26 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
 
+
+def until_iterator(number_of_times)
+  
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.do
+  until number_of_times == 0 do
+    number_of_times -=1
     puts phrase
   end
   
+
 end
 
-def times_iterator(number_of_times)
+def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.do
-    puts phrase
-  end
-end
-
-def while_iterator(number_of_times)
-  i = 0
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  while i < number_of_times do
-    puts phrase
-    i += 1
-  end
+  for i in 0..number_of_times do
+  puts phrase
   
   
+  
+  end
 end
 
